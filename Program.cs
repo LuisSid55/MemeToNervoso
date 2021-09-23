d2855a8bef1f8d4ba5900288f4204cf8a6199484
LuisSid55 <luiscitty4@gmail.com>
2021-09-23 14:01:47 -0300

    Meme To Nervoso

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MemeToNervoso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Opnião;
+            Console.WriteLine();
+            Console.Write("O que você pensa de mim? Porque tu é Otário! ");
+            Opnião = Console.ReadLine();
+            Opnião = Opnião.Remove(10);
+            Opnião = Opnião.PadLeft(4, '.');
+            Console.WriteLine();
+            Console.WriteLine("- E sabe o que penso de você?");
+            Console.WriteLine();
+            Console.WriteLine($"{Opnião}NÃO PERA.\nTô nervoso");
+            Console.WriteLine();
+        }
+    }
+}
